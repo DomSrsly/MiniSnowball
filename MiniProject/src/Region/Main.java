2b9db1c80263e15c2303516b04e941cc87890966
dominic <dominic@joes-mbp.home>
2020-11-10 16:55:23 +0100

    build up GUI

---
+++ MiniProject/src/Region/Main.java
@@ -0,0 +1,22 @@
+package Region;
+
+import javafx.application.Application;
+
+public class Main extends Application {
+
+	public static void main (String[] args) {
+	
+		launch(args);
+	}
+	
+	public void start(Stage pirmaryStage){
+		//GridPane
+			//HBox //VBox
+		//TilePane
+		
+		TilePane tp = new TilePane();
+			
+	}
+	
+
+}
